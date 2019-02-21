0ce1ea63fb155512ed7bf3fce9637074edd9549b
Denílson dos Santos Ebling <d.ebling8@gmail.com>
2019-02-21 11:15:07 -0300

    Removed unused package

--- .emacs.d/elisp/completion-engine.el
+++ .emacs.d/elisp/completion-engine.el
@@ -15,19 +15,5 @@
   :delight
   :config (counsel-mode 1))
 
-(use-package counsel-gtags
-  :after counsel
-  :delight
-  :bind (:map counsel-gtags-mode-map
-	      ("M-t" . counsel-gtags-find-definition)
-	      ("M-r" . counsel-gtags-find-reference)
-	      ("M-s" . counsel-gtags-find-symbol)
-	      ("M-," . counsel-gtags-go-backward))
-  :hook ((c-mode c++-mode java-mode asm-mode ). counsel-gtags-mode)
-  :config
-  (evil-define-key 'evil-normal-state 'counsel-gtags-mode-map (kbd "gd") 'counsel-gtags-dwim)
-  (with-eval-after-load 'company
-    (add-to-list 'company-backends 'company-gtags)))
-
 (provide 'completion-engine)
 ;;; completion-engine.el ends here
