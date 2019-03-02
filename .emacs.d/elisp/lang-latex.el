fffccef5c3d2432d4f0c0bb1553ddafb7bb43af0
Denílson dos Santos Ebling <d.ebling8@gmail.com>
2019-03-02 11:39:44 -0300

    Latex setup reworked

--- .emacs.d/elisp/lang-latex.el
+++ .emacs.d/elisp/lang-latex.el
@@ -1,31 +1,49 @@
 ;; -*- lexical-binding: t; -*-
 
-;; (use-package tex
-;;   :ensure auctex
-;;   :hook (LaTeX-mode . (latex-math-mode latex-company-backends))
-;;   :config
-;;   (setq TeX-auto-save t
-;; 	TeX-parse-self t
-;; 	TeX-save-query nil
-;; 	reftex-plug-into-AUCTeX t
-;; 	TeX-PDF-mode t))
-;;   (use-package company-bibtex
-;;     :config(setq company-bibtex-bibliography '("/home/debling/Documents/RefsBib/*.bib")))
-
-;;   (use-package company-reftex)
-;;   (use-package company-math)
-
 (use-package tex
   :ensure auctex
+  :commands (company-auctex-init)
+  :hook ((TeX-mode . flycheck-mode)
+	 (TeX-mode . turn-on-reftex)
+	 (TeX-mode . latex-math-mode)
+	 (TeX-mode . tex-company-setup)
+	 (TeX-mode . auctex-latexmk-setup)
+	 (TeX-mode . (lambda ()
+		       (setq TeX-command-default "LatexMk"))))
   :config
- (setq TeX-auto-save t
-    TeX-parse-self t
-    TeX-save-query nil
-    reftex-plug-into-AUCTeX t
-    TeX-PDF-mode t)
+  (setq TeX-auto-save		t
+	TeX-parse-self		t
+	TeX-save-query		nil
+	TeX-PDF-mode		t
+	TeX-command-default	"LatexMk"
+	;TeX-view-program-list	'(("Evince" "zathura -P %(outpage) %o"))
+	LaTeX-default-author	user-full-name)
+(add-to-list 'TeX-view-program-selection
+             '(output-pdf "Zathura"))
+  (use-package auctex-latexmk
+    :commands (auctex-latexmk-setup)
+    :config
+    (setq auctex-latexmk-inherit-TeX-PDF-mode t))
+  (use-package company-auctex)
 
- (use-package company-auctex
-   :hook (latex-mode . company-auctex-init)))
+  (use-package reftex
+    :commands (turn-on-reftex)
+    :config
+    (setq reftex-default-bibliography '("/home/dse/Documents/Bibrefs/main.bib")
+	  reftex-plug-into-AUCTeX t))
+  (use-package company-reftex)
+
+  (use-package bibtex-utils)
+  (use-package company-bibtex
+    :config
+    (setq company-bibtex-bibliography '("/home/dse/Documents/Bibrefs/main.bib")))
+
+  (use-package company-math)
+
+  (defun tex-company-setup ()
+    "docstring"
+    (add-to-list (make-local-variable 'company-backends) '(company-math company-reftex company-bibtex))
+    (company-auctex-init)))
 
 (provide 'lang-latex)
 ;;; lang-latex.el ends here
