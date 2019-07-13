fcc2ae52ef4fd154433dfb5b9f9f382b5121b98e
Denílson dos Santos Ebling <d.ebling8@gmail.com>
2019-07-13 08:01:47 -0300

    python setup reworked

--- .emacs.d/elisp/lang-python.el
+++ .emacs.d/elisp/lang-python.el
@@ -1,8 +1,28 @@
+;; -*- lexical-binding: t; -*-
+
 (use-package elpy
-  :hook (after-init . elpy-enable)
+  :init
+  (advice-add 'python-mode :before 'elpy-enable)
   :config
   (setq python-shell-interpreter "ipython"
-	python-shell-interpreter-args "-i --simple-prompt"))
+	python-shell-interpreter-args "-i --simple-prompt"
+	elpy-rpc-backend "jedi"))
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode)
+  (setq flycheck-python-flake8-executable "flake8"))
+
+  ;; enable autopep8 formatting on save
+(use-package py-autopep8
+  :hook (elpy-mode . py-autopep8-enable-on-save))
+
+(use-package pip-requirements
+  :config
+  (add-hook 'pip-requirements-mode-hook #'pip-requirements-auto-complete-setup))
+
+(use-package py-isort
+  :hook (before-save-hook . py-isort-before-save))
 
 (provide 'lang-python)
 ;;; lang-python.el ends here
