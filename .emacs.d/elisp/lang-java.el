5634eb645cc3f771deeb4f06712c17c97f7ccd73
Denílson dos Santos Ebling <d.ebling8@gmail.com>
2019-07-13 08:19:49 -0300

    added lsp-java spring-boot support

--- .emacs.d/elisp/lang-java.el
+++ .emacs.d/elisp/lang-java.el
@@ -4,7 +4,12 @@
   :after lsp
   :config
   (add-hook 'java-mode-hook #'lsp)
-  (setq lsp-java-completion-guess-arguments t))
+  (setq lsp-java-completion-guess-arguments t)
+  (require 'lsp-java-boot)
+
+  ;; to enable the lenses
+  (add-hook 'lsp-mode-hook #'lsp-lens-mode)
+  (add-hook 'java-mode-hook #'lsp-java-boot-lens-mode))
 
 (use-package dap-java
   :ensure nil)
