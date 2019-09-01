60f2d97dc176da382955e5814754e073aeac6678
Denílson dos Santos Ebling <d.ebling8@gmail.com>
2019-09-01 15:52:41 -0300

    fixed formating and removed unused options

--- .emacs.d/elisp/lang-org.el
+++ .emacs.d/elisp/lang-org.el
@@ -1,59 +1,58 @@
 ;; -*- lexical-binding: t; -*-
-
 (use-package org
   :hook (org-mode . org-indent-mode)
   :bind (("C-c l" . org-store-link)
 	 ("C-c a" . org-agenda)
 	 ("C-c c" . org-capture))
+  :functions (evil-define-key)
+  :defines (org-plantuml-jar-path
+	    flycheck-plantuml-executable
+	    org-capture-templates
+	    evil-normal-state-map
+	    evil-motion-state-map)
   :config
-  (define-key evil-normal-state-map (kbd "<remap> <evil-next-line>")		'evil-next-visual-line)
-  (define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>")	'evil-previous-visual-line)
-  (define-key evil-motion-state-map (kbd "<remap> <evil-next-line>")		'evil-next-visual-line)
-  (define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>")	'evil-previous-visual-line)
+  (define-key evil-normal-state-map (kbd "<remap> <evil-next-line>")	 'evil-next-visual-line)
+  (define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+  (define-key evil-motion-state-map (kbd "<remap> <evil-next-line>")	 'evil-next-visual-line)
+  (define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
   (evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
+
   (setq org-directory			"~/Org/"
-        org-plantuml-jar-path           "~/.local/jar/plantuml.jar"
-        flycheck-plantuml-executable    org-plantuml-jar-path
+	org-plantuml-jar-path           "~/.local/jar/plantuml.jar"
+	flycheck-plantuml-executable    org-plantuml-jar-path
 	org-confirm-babel-evaluate	nil
 	org-log-reschedule		t
 	org-log-done			t
-        org-default-notes-file		(concat org-directory "notes.org")
-        org-agenda-files		(list (concat org-directory "todo.org"))
-        org-src-fontify-natively	t
+	org-default-notes-file		(concat org-directory "notes.org")
+	org-agenda-files		(list (concat org-directory "todo.org"))
+	org-src-fontify-natively	t
 	org-pretty-entities             t
 	org-hide-emphasis-markers	t
-	org-capture-templates		'(("t" "TODO" entry (file "~/Org/todo.org")
-					   "* TODO %?\n%U" :empty-lines 1)
+	org-capture-templates		'(("p" "TODO - Personal" entry (file+headline "~/Org/todo.org" "Personal")
+					   "* TODO [#B] %?\n%U" :clock-in t :clock-resume t :empty-lines 1)
+					  ("w" "TODO - Work" entry (file+headline "~/Org/todo.org" "Work")
+					   "* TODO [#B] %?\n%U" :clock-in t :clock-resume t :empty-lines 1)
+					  ("u" "TODO - Uni" entry (file+headline "~/Org/todo.org" "University")
+					   "* TODO [#B] %?\n%U" :clock-in t :clock-resume t :empty-lines 1)
 					  ("n" "Note" entry (file "~/Org/notes.org")
-					   "* %? :NOTE:\n%U\n%a\n" :empty-lines 1)))
-  (when (window-system)
-    (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans pro"))
-				 ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-				 (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-	   (headline           `(:inherit default :weight bold)))
-
-      (custom-theme-set-faces
-       'user
-       `(org-level-8 ((t (,@headline ,@variable-tuple))))
-       `(org-level-7 ((t (,@headline ,@variable-tuple))))
-       `(org-level-6 ((t (,@headline ,@variable-tuple))))
-       `(org-level-5 ((t (,@headline ,@variable-tuple))))
-       `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-       `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-       `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-       `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-       `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil)))))))
+					   "* %? :NOTE:\n%U\n%a\n" :empty-lines 1)
+					  ("j" "Journal" entry (file+datetree "~/Org/jornal.org")
+					   "* %?\n%U\n" :clock-resume t)))
+  (font-lock-add-keywords 'org-mode
+			  '(("^ +\\([-*]\\) "
+			     (0 (prog1 ()
+				  (compose-region (match-beginning 1) (match-end 1) "•"))))))
   ;;; org-plus-contrib config
-    (org-babel-do-load-languages 'org-babel-load-languages '((emacs-lisp . t)
-                                                             (shell . t)
-                                                             (python . t)
-                                                             (C . t)
-                                                             (sql . t)
-							     (dot . t)
-							     (plantuml . t)
-							     (haskell . t)
-							     (clojure . t)
-							     (calc . t))))
+  (org-babel-do-load-languages 'org-babel-load-languages '((emacs-lisp . t)
+							   (shell . t)
+							   (python . t)
+							   (C . t)
+							   (sql . t)
+							   (dot . t)
+							   (plantuml . t)
+							   (haskell . t)
+							   (clojure . t)
+							   (calc . t))))
 
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode))
