d22056013c26919f98efea9b6ab1d6e78404c014
Denílson dos Santos Ebling <d.ebling8@gmail.com>
2019-09-20 11:39:44 -0300

    (emacs) lang-misc: added packages to edit nginx and mutt configs

--- .emacs.d/elisp/lang-misc.el
+++ .emacs.d/elisp/lang-misc.el
@@ -5,5 +5,14 @@
     (setq flycheck-plantuml-executable "/home/dse/plantuml.jar")
       (flycheck-plantuml-setup)))
 
+(use-package nginx-mode
+  :config
+  (use-package company-nginx
+    :config
+    (eval-after-load 'nginx-mode
+      '(add-hook 'nginx-mode-hook #'company-nginx-keywords))))
+
+(use-package mutt-mode)
+
 (provide 'lang-misc)
 ;;; lang-misc.el ends here
