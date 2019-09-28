f748fef2facb9dfe373f8e232a5db4a4661ca46c
Denílson dos Santos Ebling <d.ebling8@gmail.com>
2019-09-28 20:27:37 -0300

    (emacs) elisp: added eval-and-replace function

--- .emacs.d/elisp/lang-elisp.el
+++ .emacs.d/elisp/lang-elisp.el
@@ -2,10 +2,21 @@
 
 (use-package ielm
   :functions elisp-company-backends
-  :bind ("C-c e i" . ielm)
+  :bind (("C-c e i" . ielm)
+	 ("C-c C-v C-w" . eval-and-replace))
   :hook ((emacs-lisp-mode . elisp-company-backends)
 	 (ielm-mode . elisp-company-backends))
   :config
+  (defun eval-and-replace ()
+    "Replace the preceding sexp with its value."
+    (interactive)
+    (backward-kill-sexp)
+    (condition-case nil
+	(prin1 (eval (read (current-kill 0)))
+	       (current-buffer))
+      (error (message "Invalid expression")
+	     (insert (current-kill 0)))))
+
   (defun elisp-company-backends ()
     (with-eval-after-load 'company-mode
       (add-to-list (make-local-variable 'company-backends) '(company-elisp)))))
@@ -14,6 +25,7 @@
   :functions eros-mode
   :hook ((lisp-mode . eros-mode)
 	 (emacs-lisp-mode . eros-mode)))
+
 
 (provide 'lang-elisp)
 ;;; lang-elisp.el ends here
